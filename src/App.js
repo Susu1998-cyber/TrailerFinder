5362bad1045937bcc96cede092ebf1e2411bc4a1
unknown <susanthsankar1998@gmail.com>
2024-02-03 23:00:08 +0530

    This is my new MovieTrailerFinder app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
 
 function App() {
+  const [video, setVideo] = useState("inception");
+  const [videoURL, setVideoURL] = useState("https://youtu.be/ee9i6oMqShk?si=ompLbcJlbwd5D1m3");
+
+  function handleSearch() {
+    movieTrailer(video).then((res) => {
+      setVideoURL(res);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 style={{ color: "green" }}>MovieTrailerFinder</h1>
+
+      <div className="search-box">
+        <label>Search for any movies/shows: </label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setVideo(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            handleSearch();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </div>
+      <ReactPlayer url={videoURL} controls={true} />
     </div>
   );
 }
